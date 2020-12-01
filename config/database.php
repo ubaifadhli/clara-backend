e4fb99915fa873a77752fa2c12dbfa1611ecbff6
Kuasa5Indra <adityaindra135@gmail.com>
2020-12-01 14:34:11 +0700

    update database config

--- config/database.php
+++ config/database.php
@@ -6,7 +6,10 @@
                 'driver' => 'mongodb',
                 'dsn'=>env('DB_DSN', 'mongodb+srv://ubai_admin:gVh784U3a7!3@cluster0.nvfrn.mongodb.net/?retryWrites=true&w=majority'),
                 'database' => env('DB_DATABASE', 'clara'),
-            ]   
+                'options' => [
+                    'tls' => true,
+                ],
+            ],   
         ],
         'migrations' => 'migrations',
     ];
