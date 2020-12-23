b161311cf992d33a5ce815299706c04886cf7b92
ubaifadhli <ubaifadhli@it.student.pens.ac.id>
2020-12-23 22:42:07 +0700

    Removed sensitive credential from config.

--- config/database.php
+++ config/database.php
@@ -4,8 +4,8 @@
         'connections' => [
             'mongodb' => [
                 'driver' => 'mongodb',
-                'dsn'=>env('DB_DSN', 'mongodb+srv://ubai_admin:gVh784U3a7!3@cluster0.nvfrn.mongodb.net/?retryWrites=true&w=majority'),
-                'database' => env('DB_DATABASE', 'clara'),
+                'dsn'=>env('DB_DSN', ''),
+                'database' => env('DB_DATABASE', ''),
                 'options' => [
                     'tls' => true,
                 ],
